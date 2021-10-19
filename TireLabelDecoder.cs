7ec85d262d3d59cf1fd0f22aaf7a525f04aca89e
Krzysztof Lichwiarz <skundlony@protonmail.com>
2021-10-19 14:24:32 +0200

    NameChange

---
+++ TireLabelDecoder.cs
@@ -0,0 +1,39 @@
+﻿using Patagames.Ocr;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Tire_Label_Decoder.Types;
+
+namespace Tire_Label_Decoder
+{
+    class ValueRegion
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        Bitmap Region { get; set; }
+        string TireLabelAttribute { get; set; }
+    }
+
+    public class TireLabelDecoder
+    {
+        public static TireLabel ProcessLabel()
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init("", "eng");
+                using (var bmp = Image.FromFile(@"C:\test.jpg") as Bitmap)
+                {
+                    string plainText = api.GetTextFromImage(bmp);
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<ValueRegion> SliceBitMapIntoRegions()
+        {
+            // Bitmap.Clone 
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Types/TireLabel.cs
@@ -0,0 +1,35 @@
+﻿namespace Tire_Label_Decoder.Types
+{
+    public enum Index
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+
+    public enum Season
+    {
+        SUMMER,
+        WINTER,
+        ALL
+    }
+
+    public class TireLabel
+    {
+        public string Eprel { get; set; }
+        public string Brand { get; set; }
+        public int Width { get; set; }
+        public int Sidewall { get; set; }
+        public int Diameter { get; set; }
+        public int NoiseRating { get; set; }
+        public Index WetGrip { get; set; }
+        public Index FuelEfficiency { get; set; }
+
+        internal string GetSize()
+        {
+            return $"{Width}/{Sidewall}/{Diameter}";
+        }
+    }
+}
